501bf153c2ccafd7c2cc42d03add6d90a551a201
saif580 <saifulhasan580@gmail.com>
2021-11-19 18:48:54 +0530

    basic operation

---
+++ Arrays/Basic operations/basic.js
@@ -0,0 +1,17 @@
+//Adding an element at the end of array o(1)
+let array=[1,2]
+array.push(3)
+console.log(array)
+
+//Adding an element at the start of array o(n)
+array.unshift(0)
+console.log(array)
+
+
+//removing an element from the starting
+array.shift()
+console.log(array)
+
+//removing an element from the end
+array.pop()
+console.log(array)
