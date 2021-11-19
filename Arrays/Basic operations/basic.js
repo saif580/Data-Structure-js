136331bae412ecd05f87a9aa744ad3cbb8d20b96
saif580 <saifulhasan580@gmail.com>
2021-11-19 19:11:59 +0530

    adding and reomving from a specific position

--- Arrays/Basic operations/basic.js
+++ Arrays/Basic operations/basic.js
@@ -15,3 +15,17 @@ console.log(array)
 //removing an element from the end
 array.pop()
 console.log(array)
+
+//Add and remove from specific position
+//splice method is use
+//Syntax-arr.splice(start,delete,item1,item2,item3......item n)
+
+
+//To add element from a specific position
+let array1=[21,213,432,12,345,453]
+array1.splice(1,0,1,2,3)
+console.log(array1)
+
+//Removing element from a specific position
+array1.splice(1,3)
+console.log(array1)
