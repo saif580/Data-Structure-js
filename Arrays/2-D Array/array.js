47f8e4d65a17a4b84c3f7f021fa950e7004c129f
saif580 <saifulhasan580@gmail.com>
2021-11-20 02:36:35 +0530

    2-d array

---
+++ Arrays/2-D Array/array.js
@@ -0,0 +1,11 @@
+let matrice=[]
+let rows=5
+let column=5
+for(let i=0;i<rows;i++){
+    matrice[i]=[];
+    for(let j=0;j<column;j++){
+        matrice[i][j]=null
+    }
+}
+
+console.log(matrice[1][1])
