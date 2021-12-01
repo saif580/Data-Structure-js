c67f74b50ad897100f911e9864d0827c510880d0
saif580 <saifulhasan580@gmail.com>
2021-12-01 14:36:54 +0530

    printing linklist

--- LinkedList/link.js
+++ LinkedList/link.js
@@ -17,7 +17,20 @@ class LinkedList {
         this.tail=newNode;
         this.length+=1;
     }
+
+    print(){
+        let arr=[]
+        let current=this.head;
+        while(current!=null){
+            arr.push(current.value)
+            current=current.next
+        }
+        return console.log(arr)
+    }
 }
 
 const Node=new LinkedList(10)
+Node.appendAtEnd(20)
+Node.appendAtEnd(30)
+Node.print()
 console.log(Node)
